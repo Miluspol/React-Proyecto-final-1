e0c074937ca1595c2f6668f7fa24db20682c4790
Milagros poletti <milagroselizabethpoletti@gmail.com>
2023-11-23 12:32:34 -0300

    eliminando lineas
    eliminando lineas y guardando el codigo

--- react-proyectofinal-argprog/src/App.js
+++ react-proyectofinal-argprog/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import "./App.css";
+
 
 function App() {
+
+  
+  const [tareasItems, setTareasItems] = useState([])
+  const [showCompleted, setShowCompleted] = useState(false)
+
+//funcion que añade nueva tarea
+  function crearTarea(tareaName){
+    if (!tareasItems.find(tarea => tarea.name === tareaName)){
+      setTareasItems([...tareasItems,{name: tareaName, done:false}])
+    }
+    else{
+      alert("Esa tarea ya existe")
+    }
+    
+  };
+
+//función que cambia la propiedad done a true o false. Esta funcion va a se pasada como propiedad a los componentes
+  const alternarTarea = tarea => {
+    setTareasItems(
+      tareasItems.map(t => (t.name === tarea.name) ? {...tarea, done: !t.done}: t)
+    );
+  };
+
+//cuando reinicio la pág. los items quedan guardados
+  useEffect(() => {
+    let datos = localStorage.getItem('tareas')
+    if (datos){
+      setTareasItems(JSON.parse(datos))
+    }
+    
+  }, []);
+
+  //Función eliminr tarea
+  const eliminarTareas = () =>{
+    setTareasItems(tareasItems.filter(tarea => !tarea.done))
+    setShowCompleted(false)
+  }
+
+//guarda los items en el localStorage
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareasItems))
+  }, [tareasItems]);
+
+  // showCompleted encerrado entre {} muestra la tabla de tareas completas cuando le das click al checkbox
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreadorDeTareas crearTarea={crearTarea}/>
+      <TablaDeTareas tareas={tareasItems} alternarTarea={alternarTarea}/>
+      <ControlesVisibles
+      isChecked={showCompleted}
+      setShowCompleted={(checked)=> setShowCompleted(checked)}
+      eliminarTareas={eliminarTareas}
+      />
+      {
+        showCompleted === true && (
+          <TablaDeTareas tareas={tareasItems} alternarTarea={alternarTarea} showCompleted= {showCompleted} />
+        )
+      }
     </div>
   );
-}
+};
 
 export default App;
